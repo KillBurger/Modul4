04b7078e7e6a12aa8d29284487c8c58b140b8982
WINDOWS 10 <WINDOWS 10@Windows10>
2018-10-04 22:00:02 +0700

    Latihan dan tugas mod4

---
+++ src/Manusia/LakiLaki.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Manusia;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class LakiLaki extends Manusia {
+    //constructor
+    public LakiLaki (double TB)
+    {
+        super (TB);
+    }
+
+    //method HtgBBI () merupakan method overidding dari superclass nya
+    public double HtgBBI()
+    {
+        return (super.getTB()-100)*0.9;
+  
+    }
+    
+}
+    
+
